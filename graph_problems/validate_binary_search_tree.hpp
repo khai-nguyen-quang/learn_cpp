85f0c6e0dbefa61f08b230a9eeb14db76cb13175
Khai NguyenQuang <quangkhai1808@gmail.com>
2024-10-31 17:33:38 +0700

    Improvement for leetcode 98.Validate Binary Tree using set

--- graph_problems/validate_binary_search_tree.hpp
+++ graph_problems/validate_binary_search_tree.hpp
@@ -4,7 +4,7 @@
 
 #include "common.hpp"
 #include <iostream>
-#include <vector>
+#include <set>
 #include <algorithm>
 
 using namespace std;
@@ -13,7 +13,7 @@ private:
     const bool side_left{0};
     const bool side_right{1};
 
-    void dfs(TreeNode* node, const vector<int>& list_greater, const vector<int>& list_smaller, bool& valid) {
+    void dfs(TreeNode* node, set<int>& list_greater, set<int>& list_smaller, bool& valid) {
         if (!node) return;
         if (!valid) return;
 
@@ -21,47 +21,40 @@ private:
             node->val is expected to greater than every items in list_smaller
             node->val is expected to smaller than every items in list_greater
         */
-       if  (find_if(list_greater.begin(), list_greater.end(), 
-                    [=](const int& val){ 
-                        return (val <= node->val);
-                    }
-                ) != list_greater.end()
-            ) 
-            {
-                valid = false;
-            }
-        
-        if  (find_if(list_smaller.begin(), list_smaller.end(), 
-                    [=](const int& val){ 
-                        return (val >= node->val);
-                    }
-                ) != list_smaller.end()
-            ) 
-            {
-                valid = false;
-            }
+        if (!list_greater.empty()){
+            valid = valid && node->val < *list_greater.begin();
+        }
+        if (!list_smaller.empty()) {
+            valid = valid && node->val > *list_smaller.rbegin();
+        }
         if (!valid) return;
 
-        vector<int> list_greater_new{list_greater};
-        vector<int> list_smaller_new{list_smaller};
-        list_greater_new.push_back(node->val);
-        list_smaller_new.push_back(node->val);
-        dfs(node->left, list_greater_new, list_smaller, valid);
-        dfs(node->right, list_greater, list_smaller_new, valid);
+        if (node->left) {
+            list_greater.insert(node->val);
+            dfs(node->left, list_greater, list_smaller, valid);
+            list_greater.erase(node->val);
+            if (!valid) return;
+        }
+        if (node->right){
+            list_smaller.insert(node->val);
+            dfs(node->right, list_greater, list_smaller, valid);
+            list_smaller.erase(node->val);
+            if (!valid) return;
+        }   
     }
 
 public:
     bool isValidBST(TreeNode* root) {
         bool valid{true};
-        vector<int> list_greater_l;
-        vector<int> list_smaller_l;
-        vector<int> list_greater_r;
-        vector<int> list_smaller_r;
+        set<int> list_greater_l;
+        set<int> list_smaller_l;
+        set<int> list_greater_r;
+        set<int> list_smaller_r;
 
-        list_greater_l.push_back(root->val);
-        list_smaller_r.push_back(root->val);
-        dfs(root->left,  list_greater_l, vector<int>(), valid);
-        dfs(root->right, vector<int>(), list_smaller_r, valid);
+        list_greater_l.insert(root->val);
+        list_smaller_r.insert(root->val);
+        dfs(root->left,  list_greater_l, list_smaller_l, valid);
+        dfs(root->right, list_greater_r, list_smaller_r, valid);
         return valid;
     }
 };
